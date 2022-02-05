27f463af24296072bf84c7a43216637203941aae
YxZi5 <93923490+YxZi5@users.noreply.github.com>
2022-02-05 17:30:15 +0100

    Update check_country.py

--- check_country.py
+++ check_country.py
@@ -1,4 +1,7 @@
 #!/bin/usr/python3
+#if script not working type apt install geoip-bin in console 
+#Or check localization your file with addreses: adresy
+
 import subprocess as sp
 
 adresy = open("out1.txt", "r")
